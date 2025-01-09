873492634425093563b5ee4617306f40993b8afd
Richmond Baltazar <baltazarrichmond31@gmail.com>
2025-01-09 17:25:18 +0800

    Fixed text input max line

--- component/CustomTextInput.tsx
+++ component/CustomTextInput.tsx
@@ -1,4 +1,4 @@
-import { TextInput, TouchableOpacity, View } from "react-native";
+import { TextInput, TouchableOpacity, View, StyleSheet } from "react-native";
 import React from "react";
 import {
   widthPercentageToDP as wp,
@@ -29,12 +29,14 @@ const CustomTextInput = ({
         marginVertical: hp(1),
         flexDirection: "row",
         backgroundColor: "#F8F9FA",
+        alignItems: "center",
       }}
     >
       <TextInput
         placeholder={placeholder}
-        style={{ flex: 1 }}
+        style={{ flex: 1, fontSize: wp(5) }}
         secureTextEntry={isPasswordHidden}
+        numberOfLines={1}
       />
       {isPassword && (
         <TouchableOpacity
@@ -42,19 +44,20 @@ const CustomTextInput = ({
           onPress={() => setIsPasswordHidden!(!isPasswordHidden)}
         >
           {isPasswordHidden ? (
-
             <Feather
               name="eye-off"
-              size={wp(4.5)}
+              size={wp(6)}
               color="black"
-              style={{ paddingHorizontal: wp(1) }}
+              style={styleSheet.eyeIconStyle}
             />
-          ) : <Feather
-            name="eye"
-            size={wp(4.5)}
-            color="black"
-            style={{ paddingHorizontal: wp(1) }}
-          />}
+          ) : (
+            <Feather
+              name="eye"
+              size={wp(6)}
+              color="black"
+              style={styleSheet.eyeIconStyle}
+            />
+          )}
         </TouchableOpacity>
       )}
     </View>
@@ -62,3 +65,9 @@ const CustomTextInput = ({
 };
 
 export default CustomTextInput;
+
+const styleSheet = StyleSheet.create({
+  eyeIconStyle: {
+    paddingHorizontal: wp(2),
+  },
+});
