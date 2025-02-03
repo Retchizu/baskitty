8134c97652e339d458589060a0bac0dd75c1b344
Erlyn De Leon <erlynqueendeleon81@gmail.com>
2025-02-04 01:04:41 +0800

    Profile UI Draft

--- src/initSupabase.ts
+++ src/initSupabase.ts
@@ -3,6 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 import { BASKITTY_API_KEY, BASKITTY_PROJECT_URL } from "@env";
 const supabaseUrl = BASKITTY_PROJECT_URL;
 const supabaseKey = BASKITTY_API_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     storage: AsyncStorage,
